7712c0eb9e7e77ae59423c7110cd392926499f89
ke7gyq <ke7gyq@gmail.com>
2016-08-07 16:08:09 -0700

    Bug fix

--- Controller/Widgets.py
+++ Controller/Widgets.py
@@ -12,7 +12,7 @@ master = Tk()
 # 
 def query ( robot, qString ):
     robot.send(qString)
-    time.sleep(.1)
+    time.sleep(.3)
     return robot.get()
 
 
@@ -24,6 +24,53 @@ def stringToTriplet( string ):
     print "Map Value %s, String %s" % (mapValue, string)
     return mapValue
 
+def stringToFloatTriplet(string):
+    string = string.strip()
+    mapValue = map (float, string[1:-1].split())
+    print "Map Value %s, String %s" % (mapValue, string)
+    return mapValue
+    
+
+
+class CameraGains:
+    def __init__(self, master, robot):
+        self.master , self.robot = master, robot
+    
+        topFrame = Frame(master)
+        topFrame.pack()
+        frame = Frame(topFrame)
+        frame.pack()
+        scroll =Frame(frame)
+        scroll.pack()
+
+
+        redGain  = Scale(scroll, from_=0, to=8.0, resolution=0.01)
+        redGain.pack(side=LEFT)
+        blueGain = Scale(scroll,from_=0, to=8.0,resolution=0.01)
+        blueGain.pack(side=LEFT)
+        iso = Scale ( scroll, from_=0, to= 800,resolution=200)
+        iso.pack(side=LEFT)
+
+        setValues = Button (frame, text="Set Camera Gains", 
+                            command= lambda : self.setValues())
+        setValues.pack(side=LEFT,expand=True)
+        getValues = Button (frame, text='Get Camera Gains',
+                            command=lambda : self.getValues())
+        getValues.pack(side=LEFT,expand=True)
+        frame.pack()
+
+        self.scales  = [redGain, blueGain, iso]
+        self.getValues()
+
+    def setValues(self):
+        rg,bg,iso = [ v.get() for v in self.scales]
+        self.robot.send("setcamerascales %f %f %d\n"% (rg,bg,iso))
+
+    def getValues(self):
+        scales = query (self.robot, 'getcamerascales\n')
+        vals = stringToFloatTriplet (scales)
+        for v,o in zip ( vals, self.scales):
+            o.set(v)
 
 class ColorBallance:
     def __init__(self, master, robot):
@@ -34,7 +81,7 @@ class ColorBallance:
         topFrame.pack()
         frame = Frame(topFrame)
         frame.pack()
-        scroll =Frame(frame, background="tan")
+        scroll =Frame(frame)
         scroll.pack()
 
         hueLow = Scale(scroll, from_=255, to=0)
@@ -53,10 +100,10 @@ class ColorBallance:
         valHigh.pack(side=LEFT)
         scroll.pack()
 
-        setValues = Button (frame, text="SetValues", 
+        setValues = Button (frame, text="Set HSV", 
                             command= lambda : self.setValues())
         setValues.pack(side=LEFT,expand=True)
-        getValues = Button (frame, text='Get Values',
+        getValues = Button (frame, text='Get HSV',
                             command=lambda : self.getValues())
         getValues.pack(side=LEFT,expand=True)
         frame.pack()
@@ -71,7 +118,7 @@ class ColorBallance:
         lh,ls,lv = [ v.get() for v in self.lowScales]
         hh,hs,hv = [ v.get() for v in self.highScales]
         self.robot.send( "setlow %d %d %d\n" %( lh,ls,lv ))
-        time.sleep(.1)
+        time.sleep(.3)
         self.robot.send( "sethigh %d %d %d\n" %(hh,hs,hv))
         
     def getValues(self):
@@ -95,6 +142,7 @@ class Widgets(threading.Thread):
     def run(self):
         master = Tk()
         cb = ColorBallance(master, self.robot)
+        cg = CameraGains( master, self.robot)
         master.mainloop()
 
 def initWidgets ( robot ) :
